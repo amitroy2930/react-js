113e0bce9699f6fd89c5e8e919c757d0014d3835
Amit Roy <amitroy2930@gmail.com>
2023-10-25 17:42:24 +0530

    update 01vitereact

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+    return (
+        <>
+            <h1>Custom App | Chai</h1>
+        </>
+    )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
+    // MyApp() -> It is also working
+    //<MyApp/> -> It is also working
 )
